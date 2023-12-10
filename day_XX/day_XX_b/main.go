38fe75f62fbb02975dbeb77613562a750ba7f51a
Carlos Ruiz Herrera <carlos.ruiz.herrera@rock.estate>
2023-12-10 15:43:24 +0100

    Include parser in template

--- day_XX/day_XX_b/main.go
+++ day_XX/day_XX_b/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -11,6 +12,61 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+type parser struct {
+	input []rune
+	pos   int
+	done  bool
+}
+
+func newParser(input string) parser {
+	p := parser{[]rune(input), -1, false}
+	p.next()
+	return p
+}
+
+func (p *parser) next() (rune, bool) {
+	if p.pos == len(p.input)-1 {
+		p.done = true
+		return p.input[p.pos], false
+	}
+	p.pos++
+	return p.input[p.pos], true
+}
+
+func (p *parser) current() (rune, bool) {
+	return p.input[p.pos], !p.done
+}
+
+func (p *parser) skip() int {
+	amount := 0
+	for r, ok := p.current(); ok; r, ok = p.next() {
+		if !(r == ' ') {
+			break
+		}
+		amount++
+	}
+	return amount
+}
+
+func (p *parser) parseInt() int {
+	r, _ := p.current()
+	out := string(r)
+	for r, ok := p.next(); ok; r, ok = p.next() {
+		if r >= '0' && r <= '9' {
+			out += string(r)
+		} else {
+			break
+		}
+	}
+
+	id, err := strconv.Atoi(out)
+
+	if err != nil {
+		panic(err)
+	}
+	return id
 }
 
 func main() {
